af1485cc7b441ab297e2d89951cee608bec51f5b
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2022-10-13 06:29:18 +0530

    Update githubAPI_test.py

--- githubAPI_test.py
+++ githubAPI_test.py
@@ -6,7 +6,7 @@ from githubAPI567_Atishay import Github
 class TestGithub(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
     def test1(self):
-        self.assertEqual(Github('kdoskdowkdowkdowk'), , False)
+        self.assertEqual(Github('kdoskdowkdowkdowk'), False)
     def test2(self):
         self.assertEqual(Github('atishay2305-hub'), True)
         self.assertEqual(Github('sdsjdosjdosjdsd'), False)

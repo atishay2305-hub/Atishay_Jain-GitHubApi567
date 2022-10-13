09d184ab16b81fb5d12e44772c5ec8acda78accd
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2022-10-13 06:10:50 +0530

    Update githubAPI_test.py

--- githubAPI_test.py
+++ githubAPI_test.py
@@ -1,29 +1,35 @@
+#Author : Atishay Jain
+#Subject : 567A 
+#HW 05A: Isolate External Dependencies
+
+#Mock the Github API
+
+
+
 import unittest
 from unittest import TestCase
 from unittest import mock
-
+from unittest.mock import patch, MagicMock, Mock
 from rx import return_value
 import githubAPI567_Atishay
 from githubAPI567_Atishay import Github
 
 
-from githubAPI567_Atishay import Github
-
-@mock.patch('githubAPI567_Atishay.Github', return_value=False)
 class TestGithub(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
-    def test1(self, mock_github_results):
-        actual_result = githubAPI567_Atishay.Github()
-        expected_result = False
-        self.assertEqual(expected_result, actual_result)
-        
+    @mock.patch('githubAPI567_Atishay.Github', return_value = True)
+    def test1(self, mock_github_results): 
+        original_value = githubAPI567_Atishay.Github()
+        expected_value = True
+        self.assertEqual(expected_value, original_value)
+
+    @mock.patch('githubAPI567_Atishay.Github', return_value = True)    
     def test2(self, mock_github_results):
-        actual_directory = githubAPI567_Atishay.Github()
-        expected_directory = True
-        self.assertNotEqual(expected_directory, actual_directory)
+        original_file = githubAPI567_Atishay.Github()
+        expected_file = True
+        self.assertEqual(expected_file, original_file) 
     
+
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
 
